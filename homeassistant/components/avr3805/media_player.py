4c17546ffb4f0067111dc5258634d050045e15d8
Philip Rodning <pbr@pbr.pw>
2019-09-04 13:47:38 -0400

    change denon avr 3805 volume inc and dec to move by 3db

--- homeassistant/components/avr3805/media_player.py
+++ homeassistant/components/avr3805/media_player.py
@@ -9,38 +9,63 @@ import logging
 import voluptuous as vol
 
 from homeassistant.components.media_player import (
-    PLATFORM_SCHEMA, SUPPORT_NEXT_TRACK, SUPPORT_PAUSE, SUPPORT_PLAY,
-    SUPPORT_PREVIOUS_TRACK, SUPPORT_SELECT_SOURCE, SUPPORT_STOP,
-    SUPPORT_TURN_OFF, SUPPORT_TURN_ON, SUPPORT_VOLUME_MUTE, SUPPORT_VOLUME_SET,
-    MediaPlayerDevice)
-from homeassistant.const import (
-    CONF_HOST, CONF_NAME, STATE_OFF, STATE_ON, STATE_UNKNOWN)
+    PLATFORM_SCHEMA,
+    SUPPORT_NEXT_TRACK,
+    SUPPORT_PAUSE,
+    SUPPORT_PLAY,
+    SUPPORT_PREVIOUS_TRACK,
+    SUPPORT_SELECT_SOURCE,
+    SUPPORT_STOP,
+    SUPPORT_TURN_OFF,
+    SUPPORT_TURN_ON,
+    SUPPORT_VOLUME_MUTE,
+    SUPPORT_VOLUME_SET,
+    MediaPlayerDevice,
+)
+from homeassistant.const import CONF_HOST, CONF_NAME, STATE_OFF, STATE_ON, STATE_UNKNOWN
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['pyserial']
+REQUIREMENTS = ["pyserial"]
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = 'Music station'
+DEFAULT_NAME = "Music station"
 
-SUPPORT_DENON = SUPPORT_VOLUME_SET | SUPPORT_VOLUME_MUTE | \
-    SUPPORT_TURN_ON | SUPPORT_TURN_OFF | SUPPORT_SELECT_SOURCE \
-
+SUPPORT_DENON = (
+    SUPPORT_VOLUME_SET
+    | SUPPORT_VOLUME_MUTE
+    | SUPPORT_TURN_ON
+    | SUPPORT_TURN_OFF
+    | SUPPORT_SELECT_SOURCE
+)
 SUPPORT_MEDIA_MODES = 0
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST): cv.string,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-})
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_HOST): cv.string,
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    }
+)
 
-NORMAL_INPUTS = {'PHONO':'Phono', 'CD':'CD', 'TUNER':'Tuner',
-                 'DVD':'DVD', 'VDP':'VDP', 'TV':'TV',
-                 'Satelite / Cable':'DBS/SAT', 'VCR 1':'VCR-1',
-                 'VCR 2':'VCR-2', 'VCR 3':'VCR-3', 'Video Aux':'V.AUX',
-                 'CD-R/Tape 1':'CDR/TAPE1', 'MD/Tape 2':'MD/TAPE2',
-                 'USB':'USB', 'IPOD':'IPOD' }
+NORMAL_INPUTS = {
+    "PHONO": "Phono",
+    "CD": "CD",
+    "TUNER": "Tuner",
+    "DVD": "DVD",
+    "VDP": "VDP",
+    "TV": "TV",
+    "Satelite / Cable": "DBS/SAT",
+    "VCR 1": "VCR-1",
+    "VCR 2": "VCR-2",
+    "VCR 3": "VCR-3",
+    "Video Aux": "V.AUX",
+    "CD-R/Tape 1": "CDR/TAPE1",
+    "MD/Tape 2": "MD/TAPE2",
+    "USB": "USB",
+    "IPOD": "IPOD",
+}
 
-MEDIA_MODES = {'Tuner': 'TUNER'}
+MEDIA_MODES = {"Tuner": "TUNER"}
 
 # Sub-modes of 'NET/USB'
 # {'USB': 'USB', 'iPod Direct': 'IPD', 'Internet Radio': 'IRP',
@@ -62,62 +87,73 @@ class DenonDevice(MediaPlayerDevice):
         """Initialize the Denon device."""
         self._name = name
         self._host = host
-        self._pwstate = 'PWSTANDBY'
+        self._pwstate = "PWSTANDBY"
         self._volume = 0
         self._volume_max = 80
         self._source_list = NORMAL_INPUTS.copy()
         self._source_list.update(MEDIA_MODES)
         self._muted = False
-        self._mediasource = ''
-        self._mediainfo = ''
+        self._mediasource = ""
+        self._mediainfo = ""
 
     @classmethod
     def serial_request(cls, serial, command, all_lines=False):
         """Execute `command` and return the response."""
         _LOGGER.debug("Sending: %s", command)
-        serial.write(command.encode('ASCII') + b'\r')
+        serial.write(command.encode("ASCII") + b"\r")
         lines = []
         while True:
-            line = serial.read_until(b'\r')
+            line = serial.read_until(b"\r")
             if not line:
                 break
-            lines.append(line.decode('ASCII').strip())
+            lines.append(line.decode("ASCII").strip())
             _LOGGER.debug("Received: %s", line)
 
         if all_lines:
             return lines
-        return lines[0] if lines else ''
+        return lines[0] if lines else ""
 
     def serial_command(self, command):
         import serial
+
         """Establish a telnet connection and sends `command`."""
         telnet = serial.Serial(self._host, 9600, timeout=0.2)
         _LOGGER.debug("Sending: %s", command)
-        telnet.write(command.encode('ASCII') + b'\r')
+        telnet.write(command.encode("ASCII") + b"\r")
         telnet.read()  # skip response
         telnet.close()
 
     def update(self):
         import serial
+
         """Get the latest details from the device."""
         try:
             telnet = serial.Serial(self._host, 9600, timeout=0.2)
         except OSError:
             return False
 
-        self._pwstate = self.serial_request(telnet, 'PW?')
-        for line in self.serial_request(telnet, 'MV?', all_lines=True):
-            if line.startswith('MV'):
-                self._volume = int(line[len('MV'):])
-        self._muted = (self.serial_request(telnet, 'MU?') == 'MUON')
-        self._mediasource = self.serial_request(telnet, 'SI?')[len('SI'):]
+        self._pwstate = self.serial_request(telnet, "PW?")
+        for line in self.serial_request(telnet, "MV?", all_lines=True):
+            if line.startswith("MV"):
+                self._volume = int(line[len("MV") :])
+        self._muted = self.serial_request(telnet, "MU?") == "MUON"
+        self._mediasource = self.serial_request(telnet, "SI?")[len("SI") :]
 
         if self._mediasource in MEDIA_MODES.values():
             self._mediainfo = ""
-            answer_codes = ["NSE0", "NSE1X", "NSE2X", "NSE3X", "NSE4", "NSE5",
-                            "NSE6", "NSE7", "NSE8"]
-            for line in self.serial_request(telnet, 'NSE', all_lines=True):
-                self._mediainfo += line[len(answer_codes.pop(0)):] + '\n'
+            answer_codes = [
+                "NSE0",
+                "NSE1X",
+                "NSE2X",
+                "NSE3X",
+                "NSE4",
+                "NSE5",
+                "NSE6",
+                "NSE7",
+                "NSE8",
+            ]
+            for line in self.serial_request(telnet, "NSE", all_lines=True):
+                self._mediainfo += line[len(answer_codes.pop(0)) :] + "\n"
         else:
             self._mediainfo = self.source
 
@@ -132,9 +168,9 @@ class DenonDevice(MediaPlayerDevice):
     @property
     def state(self):
         """Return the state of the device."""
-        if self._pwstate == 'PWSTANDBY':
+        if self._pwstate == "PWSTANDBY":
             return STATE_OFF
-        if self._pwstate == 'PWON':
+        if self._pwstate == "PWON":
             return STATE_ON
 
         return STATE_UNKNOWN
@@ -175,29 +211,28 @@ class DenonDevice(MediaPlayerDevice):
 
     def turn_off(self):
         """Turn off media player."""
-        self.serial_command('PWSTANDBY')
+        self.serial_command("PWSTANDBY")
 
     def volume_up(self):
         """Volume up media player."""
-        self.serial_command('MVUP')
+        self.set_volume_level(self._volume + 3)
 
     def volume_down(self):
         """Volume down media player."""
-        self.serial_command('MVDOWN')
+        self.set_volume_level(self._volume - 3)
 
     def set_volume_level(self, volume):
         """Set volume level, range 0..1."""
-        self.serial_command('MV' +
-                            str(round(volume * self._volume_max)).zfill(2))
+        self.serial_command("MV" + str(round(volume * self._volume_max)).zfill(2))
 
     def mute_volume(self, mute):
         """Mute (true) or unmute (false) media player."""
-        self.serial_command('MU' + ('ON' if mute else 'OFF'))
+        self.serial_command("MU" + ("ON" if mute else "OFF"))
 
     def turn_on(self):
         """Turn the media player on."""
-        self.serial_command('PWON')
+        self.serial_command("PWON")
 
     def select_source(self, source):
         """Select input source."""
-        self.serial_command('SI' + self._source_list.get(source))
+        self.serial_command("SI" + self._source_list.get(source))
